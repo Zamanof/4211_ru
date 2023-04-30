2a5d769dbcc0d482d8ac86f7ab4535322d8c3eab
Zamanof <nadir.r.zamanov@gmail.com>
2023-04-30 12:02:24 +0400

    Js Intro

---
+++ JavaScript/Lesson 1/myscript.js
@@ -0,0 +1,169 @@
+// console.log("Salam")
+/*
+This is JS document
+JS is most popular client side script language.
+*/
+
+/*
+1. Undefined - имеет одно значение undefined
+
+2. Null -  имеет одно значение null
+
+3. Boolean - true, false
+
+4. String - строка
+
+5. Number - числовой тип - (5e-324 - 1.7976931348623157e+308)
+    специальные значения для Number
+    - "NaN" - not a number
+    - "Infinity"
+    - "-Infinity"
+    - "-0"
+
+6. Object
+
+7. Symbol
+
+*/
+
+/*
+Арифметические операторы
++
+-
+*
+/
+%
+++
+--
+ */
+
+
+/*
+Операторы отношений
+>
+<
+>=
+<=
+==
+!=
+===
+!==
+ */
+
+/*
+логические операции
+&& - логический и
+|| - логическое или
+! - логическое отрицание
+
+побитовые операции
+&
+2 & 3 = 2
+    0 0 0 0 0 0 1 0
+&
+    0 0 0 0 0 0 1 1
+    ---------------
+    0 0 0 0 0 0 1 0
+|
+6 | 3 = 7
+    0 0 0 0 0 1 1 0
+|
+    0 0 0 0 0 0 1 1
+    ---------------
+    0 0 0 0 0 1 1 1
+^
+6 ^ 3 = 7
+    0 0 0 0 0 1 1 0
+^
+    0 0 0 0 0 0 1 1
+    ---------------
+    0 0 0 0 0 1 0 1
+~ -
+
+>>
+<<
+*/
+
+// u = x = y = z = 5
+// console.log(u)
+// console.log(y)
+// console.log(z)
+// console.log(x)
+
+// alert() - вывод предупреждения
+// prompt() - ввод через промпт окно браузера
+
+// Условия
+// numb = prompt("Enter any number: ")
+// console.log(typeof (numb))
+// console.log(typeof (Number.parseInt(numb)))
+// if (numb < 3){25
+//     // console.log("Salam")
+//     alert("Salam")
+// }
+// else{
+//     // console.log("Hi")
+//     alert("Hi")
+// }
+
+// Ternary
+// numb = 5
+//
+// numb > 3 ? console.log("Hi"): console.log("Goodbye")
+
+// switch
+
+// numb = +prompt("Enter any number:")
+// switch (numb){
+//     case 1: console.log("Salam"); break;
+//     case 2: console.log("Hi"); break;
+//     case 3: console.log("goodbye"); break;
+//     case 4: console.log("Alooo"); break;
+//     default: console.log("Default")
+// }
+
+// while
+// i = 0;
+// while (i < 10){
+//     console.log(i);
+//     i++;
+// }
+
+// i = 0
+
+// while (i !== 0){
+//     console.log("Salam")
+// }
+
+// do{
+//     console.log("Salam")
+// }while (i !== 0)
+
+
+// for
+
+// arr = [2, 9, 25, 98, 7, 1]
+// for (i = 0; i <= 5; i++){
+//     console.log(arr[i])
+// }
+
+// for (i in arr){
+//     console.log(i)
+// }
+
+// for (i of arr){
+//     console.log(i)
+// }
+
+
+// i = 0;
+//
+// while(i < 20){
+//     i++;
+//     if (i === 5){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+
