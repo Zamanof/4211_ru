41bb9ac590d9d0e7a8d37bcc27801df53e901636
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2023-06-16 10:07:59 +0400

    Razor pages intro 1

---
+++ ASP.NET lessons/Razor Page Intro/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Razor_Page_Intro.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
