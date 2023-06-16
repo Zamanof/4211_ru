d99f97dbc35956e9e5a81883f66495216b70856a
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2023-06-16 12:04:05 +0400

    Razor pages intro finish

---
+++ ASP.NET lessons/Razor Page Intro/Data/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace Razor_Page_Intro.Data;
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+}
--- ASP.NET lessons/Razor Page Intro/Pages/Index.cshtml.cs
+++ ASP.NET lessons/Razor Page Intro/Pages/Index.cshtml.cs
@@ -1,20 +1,61 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razor_Page_Intro.Data;
+using System.Text;
 
 namespace Razor_Page_Intro.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        //public IndexModel()
+        //{ }
+        public string foo(string name)
         {
-            _logger = logger;
+            return $"{name} foo";
+        }
+        //public void OnGet(string name, int age)
+        //{
+        //    ViewData["Name"] = name;
+        //    ViewData["Age"] = age;
+        //}
+
+        //public void OnGet(Person person)
+        //{
+        //    ViewData["Name"] = person.Name;
+        //    ViewData["Age"] = person.Age;
+        //}
+
+
+        //public void OnGet(string[] names)
+        //{
+        //    StringBuilder message = new StringBuilder();
+        //    foreach(string name in names)
+        //    {
+        //        message.Append($"{name} ");
+        //    }
+        //    ViewData["Name"] = message.ToString();
+        //    ViewData["Age"] = 0;
+        //}
+
+        //public ContentResult OnGet()
+        //{
+        //    return Content("My content text");
+        //}
+
+        private readonly IWebHostEnvironment _environment;
+
+        public IndexModel(IWebHostEnvironment environment)
+        {
+            _environment = environment;
         }
 
-        public void OnGet()
+        public FileResult OnGet()
         {
-
+            string path = 
+                Path.Combine(_environment.ContentRootPath, "Data/file.txt");
+            var bytes = System.IO.File.ReadAllBytes(path);
+            return File(bytes, "Application/txt", "Salam.txt");
         }
     }
 }
